d44030eb086e4b29c73e1f4a1d4839cd4986664e
skhoza023 <skhoza023@student.wethinkcode.co.za>
2024-12-04 09:00:20 +0200

    the booking endpoint of the api works as expected

--- ConvenientSelfStorage/api-test.sh
+++ ConvenientSelfStorage/api-test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 
-curl -X GET https://uq6kl9x51a.execute-api.eu-west-1.amazonaws.com/v1/storage/units
+# curl -X GET https://uq6kl9x51a.execute-api.eu-west-1.amazonaws.com/v1/storage/units
+
+curl -X POST https://uq6kl9x51a.execute-api.eu-west-1.amazonaws.com/v1/storage/units/book \
+-H "Content-Type: application/json" \
+-d '{
+  "body": "{\"unit_id\": \"unit123\", \"customer_id\": \"cust456\", \"start_date\": \"2024-12-05\", \"end_date\": \"2024-12-15\", \"payment_method\": \"card\"}"
+}'
